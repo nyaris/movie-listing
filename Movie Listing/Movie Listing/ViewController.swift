0d10600dfbdfcae130ceb914dbd5bf70456b16a1
Nina Dominique <ninadominique12@gmail.com>
2024-07-10 18:06:10 -0300

    initial

---
+++ Movie Listing/Movie Listing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Listing
+//
+//  Created by Miguel Barone on 10/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
